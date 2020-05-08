adf53626fc7af462049c1d95abeca80ca52990b7
anthony errera <anthonyehds@gmail.com>
2020-05-08 17:37:41 +0200

    first push

---
+++ nexus-api/src/main/java/com/errera/api/User.java
@@ -0,0 +1,9 @@
+package com.errera.api;
+
+public class User {
+	
+	public void execute() {
+		System.out.println("************************************************************ je suis le user de l'api sans NEXUS may version LOCAL");
+	}
+
+}
---
+++ nexus-app/src/main/java/MainApp.java
@@ -0,0 +1,12 @@
+import com.errera.api.User;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+
+		
+		new User().execute();
+		
+	}
+
+}
